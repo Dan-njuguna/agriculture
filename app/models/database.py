4ec7f0d3101036c8e108f88878d95974e9ea16fe
Dan-njuguna <njugush020@gmail.com>
2024-12-09 21:10:26 +0300

    add CropPrediction and Prediction models to enhance data validation for crop predictions

--- app/models/database.py
+++ app/models/database.py
@@ -1,2 +1,15 @@
 from pydantic import BaseModel
 from typing import List, Optional
+
+class CropPrediction(BaseModel):
+    N: float
+    P: float
+    K: float
+    temperature: float
+    humidity: float
+    ph: float
+    rainfall: float
+
+class Prediction(BaseModel):
+    prediction: List[float]
+    class_name: str
