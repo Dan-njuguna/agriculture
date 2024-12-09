6251b58dd69d0e4ef42d81006d6808528847daa9
Dan-njuguna <njugush020@gmail.com>
2024-12-09 21:10:15 +0300

    refactor app.py to improve model loading and prediction logging; update static and template paths; add prediction endpoint

--- app/app.py
+++ app/app.py
@@ -5,13 +5,20 @@ from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from fastapi.requests import Request
 from fastapi.middleware.cors import CORSMiddleware
+from app.models.database import CropPrediction
+from app.models.database import Prediction
 import pickle
 import numpy as np
 from typing import Dict
+from logging import getLogger
+import logging
 
 # Create the FastAPI app
 app = FastAPI()
+
+getLogger().setLevel('INFO')
 
 # Base directory of the project
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,20 +26,27 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODEL_PATH = os.path.join(BASE_DIR, 'model', 'agriculture_model.pkl')
 
 # Mount static files
-app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "/static")), name="static")
+app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "app/static")), name="static")
 
 # Set up Jinja2 templates
-templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "app/templates"))
 
 classes = ['apple', 'banana', 'blackgram', 'chickpea', 'coconut', 'coffee', \
        'cotton', 'grapes', 'jute', 'kidneybeans', 'lentil', 'maize', \
        'mango', 'mothbeans', 'mungbean', 'muskmelon', 'orange', 'papaya', \
        'pigeonpeas', 'pomegranate', 'rice', 'watermelon']
 
-async def predict_pipeline(factors: Dict[np.number]):
+async def predict_pipeline(factors: Dict):
     model = await get_model()
+    print("Model loaded! Predicting...")
+    try:
+        prediction = model.predict([[factors['N'], factors['P'], factors['K'], factors['temperature'], factors['humidity'], factors['ph'], factors['rainfall']]])
+        logging.info(f"Prediction: {prediction}")
+        logging.info(f"Predicted class: {classes[prediction[0]]}")
 
-    prediction = model.predict([[factors['N'], factors['P'], factors['K'], factors['temperature'], factors['humidity'], factors['ph'], factors['rainfall']]])
+    except:
+        logging.error("Error predicting the crop")
+        return "Error predicting the crop"
     return prediction, classes[prediction[0]]
 
 async def get_model():
@@ -41,6 +55,11 @@ async def get_model():
     return model
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root():
+async def read_root(request: Request):
+    return templates.TemplateResponse("home/index.html", {"request": request})
 
-    return "Hello World"
+@app.post("/predict", response_model=Prediction)
+async def predict_crop(request: Request, crop: CropPrediction):
+    prediction, class_name = await predict_pipeline(crop.dict())
+    
+    return {"prediction": prediction[0].tolist(), "class_name": class_name}
